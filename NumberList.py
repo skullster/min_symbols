288f8d5082fe2e2932feaec7eb21f11b531df83d
skullster <jfscully@gmail.com>
2023-02-13 11:15:39 +0000

    Replacement for NewCount.py

---
+++ NumberList.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+"""  TODO: Need a better name - change file name to reflect it whenever """
+"""  TODO: use a list of dictionaries - mult_a, mult_b and remainder keys """
+"""  TODO: change name - NewCount to CountList noooo ... """
+class NumberList(object):
+    def __init__(self, number):
+        self.number = number
+        self.number_list = []
+
+    def create_number_list(self):
+        prev_mult_a = 1
+        cur_mult_a = 1
+        cur_mult_b_sub = 1 
+        remainder = 0
+         
+        while True :
+            # Calculate the 2nd multiplier
+            cur_mult_b = ((self.number - self.number % cur_mult_a) // cur_mult_a) - cur_mult_b_sub
+
+            # Calculate the remainder
+            remainder = self.number % cur_mult_a + cur_mult_a * cur_mult_b_sub
+         
+            # print("Break %d %d %d" % ( cur_mult_a, cur_mult_b, (cur_mult_a * cur_mult_b) ))
+         
+            # Create list and append to owning list - just for iterations 
+            temp_list = [ cur_mult_a, cur_mult_b, remainder ]            
+            self.number_list.append( temp_list )
+        
+            # Move onto the next subtractor
+            cur_mult_b_sub += 1
+         
+            # Move onto the next multiplier ie when mult "b" is about to become the next mult "a"
+            if ( cur_mult_b == cur_mult_a ):
+                prev_mult_a = cur_mult_a
+                cur_mult_a += 1
+                cur_mult_b_sub = 1
+                if ( (self.number % cur_mult_a) > 0 ):
+                    cur_mult_b_sub = 0
+
+                # Test the second multiplier
+                test_mult_b = ((self.number - self.number % cur_mult_a) // cur_mult_a) - cur_mult_b_sub
+            
+                # Stop when mult b has already been used as mult_a - this is a repeat multiplication
+                if ( prev_mult_a >= test_mult_b ):
+                    break
+               
+                #print("%d %d" % ( cur_mult_a, cur_mult_b_sub ))
+
+    def print_list(self):
+        idx = 0
+        for a_count in self.number_list :
+            test_result = (a_count[0] * a_count[1]) + a_count[2]
+            print("%2d : %2d x %2d + %2d = %d" % (idx, a_count[0], a_count[1], a_count[2], test_result ))
+            idx+=1
+
+if __name__ == "__main__":
+    aNumber = NumberList(31)
+    aNumber.create_number_list()
+    aNumber.print_list()
+      
