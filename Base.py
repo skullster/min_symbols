e814c50259b8e88dab4ea40f20d181808db6c3ef
skullster <jfscully@gmail.com>
2023-04-17 12:23:28 +0100

    Latest additions
    Base has been extended. Symbol count for counting symbols of the given number format.
    
    Not entirely sure if I'm honest ...

--- Base.py
+++ Base.py
@@ -2,36 +2,54 @@
 
 """  TODO: Need a better name - change file name to reflect it whenever """
 class Base(object):
-   def __init__(self, base):
-      self.base = base
-      self.symbol_arr = [0]
-      self.symbol_count = len( self.symbol_arr )
-
-   def convert_number(self, number):
-      self.symbol_arr[0] = number
+    def __init__(self, base):
+        self.base = base
+        self.symbol_arr = [0]
+        self.symbol_count = len( self.symbol_arr )
+    
+    def convert_number(self, number):
+        self.symbol_arr[0] = number
       
-      while self.base < self.symbol_arr[0]:
-         print(self.symbol_arr[0], self.symbol_arr[0] % self.base)
-         self.symbol_arr.insert(1,self.symbol_arr[0] % self.base)
-         """ self.symbol_arr[0]-= self.symbol_arr[len(self.symbol_arr) - 1] """
-         self.symbol_arr[0]-= self.symbol_arr[1]
-         self.symbol_arr[0] = self.symbol_arr[0] // self.base
+        while self.base - 1 < self.symbol_arr[0]:
+            """ print(self.symbol_arr[0], self.symbol_arr[0] % self.base) """
+            self.symbol_arr.insert(1,self.symbol_arr[0] % self.base)
+            """ self.symbol_arr[0]-= self.symbol_arr[len(self.symbol_arr) - 1] """
+            self.symbol_arr[0]-= self.symbol_arr[1]
+            self.symbol_arr[0] = self.symbol_arr[0] // self.base
          
-      """ Append the MS symbol """
-      self.symbol_arr.reverse()
+        """ Append the MS symbol """
+        self.symbol_arr.reverse()
          
-   def print_conversion(self):
-      for symbol in self.symbol_arr:
-         print(symbol)
+    def print_conversion(self):
+        for symbol in reversed(self.symbol_arr):
+            print(symbol,end=':')
+        print(end='\n')
          
-   def get_symbol_count(self):
-       self.symbol_count = len( self.symbol_arr )
-       return self.symbol_count
+    def get_symbol_count(self):
+        self.symbol_count = len( self.symbol_arr )
+        return self.symbol_count
+    
+    """ get_range returns the range of the symbol count for the base. """
+    """ The intention is to use this to reduce calculation effort.    """
+    def get_range(self, symbol_count):
+        min_range = 0
+        max_range = 0
+
+        if symbol_count > 0 :
+            min_range = pow(self.base, symbol_count - 1)
+            max_range = pow(self.base, symbol_count) - 1
+         
+        return [min_range, max_range]
       
 if __name__ == "__main__":
-    aBase = Base(16)
-    aBase.convert_number(10311)
+    aBase = Base(2)
+    """ 10311 """
+    aBase.convert_number(1048576)
     aBase.print_conversion()
     
     no_symbs = aBase.get_symbol_count()
     print( no_symbs )
+    
+    range_list = aBase.get_range(39)
+    
+    print("%d %d" % (range_list[0], range_list[1]))
---
+++ SymbolCount.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 11:29:07 2023
+
+@author: jfscu
+"""
+
+""" Symbol count """
+""" - find the minimum number of symbols for different bases for a number in a * b + remainder format """
+""" - what does minimum number of symbols mean? """
+"""   base number-1 will always be 3 symbols for example 1 * base + 1 ... """
+
+""" so 3 tests: """
+    
+"""    1 : symbol counts within bases """
+"""    2 : symbol counts in binary within bases ie each symbol has the maximum binary symbols for the bases """
+"""    3 : min binary representation for each symbol AND what base that fits into! """
+import Base as B
+import NumberList as NL
+import math
+
+class SymbolCount(object):
+    def __init__(self, number):
+        self.number = number
+        self.__get_base_range()
+        self.number_list_obj = NL.NumberList(number)
+        self.number_list_obj.create_number_list()
+        self.symbol_range_list = []
+        self.file_obj = None
+
+    """ Private method """
+    """ Gets the minimum and maximum bases for the given number """
+    def __get_base_range(self):
+        self.max_base = self.number
+        
+        """ Get min base - base * base + base >= number """ 
+        """ base * base + base = base(base +1 ) > base^2 """
+        """ So get the closest square root - test and increment if less than number """
+        min_base = math.floor(math.sqrt(self.number))
+        
+        while min_base * (min_base + 1) < self.number :
+            min_base += 1
+            
+        self.min_base = min_base
+        
+    """ Get the symbol count ranges for the given base """    
+    def __symbol_count_for_base(self, base):
+        base_obj = B.Base(base)
+        base_obj.convert_number( self.number - 1 )
+        max_symbols = base_obj.get_symbol_count()
+        
+        symbol_idx = 1
+        # Add a duff 0 entry - match the number of symbols to the index
+        # It's a list of a minimum and maximum range for the base where
+        # the index is the number of symbols
+        self.symbol_range_list = [[0,0]]
+        while symbol_idx <= max_symbols :
+            self.symbol_range_list.append(base_obj.get_range(symbol_idx))
+            symbol_idx += 1
+            
+    def __get_symbol_count(self):
+        """ Base range list should be set up prior to calling this """
+        """ Get base symbol data """
+        self.__get_base_symbol_data(0)
+        
+    def __get_base_symbol_data(self, list_idx):
+        mult_a_sym_cnt = -1
+        mult_b_sym_cnt = -1
+        rem_sym_cnt = -1
+        
+        mult_a = self.number_list_obj.number_list[list_idx][0]
+        mult_b = self.number_list_obj.number_list[list_idx][1]
+        remainder = self.number_list_obj.number_list[list_idx][2]
+
+        #print( "Base symbols count %2d : %2d,  %2d,  %2d" % (list_idx, mult_a, mult_b, remainder ), file=self.file_obj )
+        
+        """ range idx matches symbol count - ignore 0 idx """
+        cnt_idx = 1
+        range_length = len(self.symbol_range_list)
+        
+        print( "Range length %d" % (range_length))
+        while cnt_idx < range_length :
+            min_range = self.symbol_range_list[cnt_idx][0]
+            max_range = self.symbol_range_list[cnt_idx][1]
+            if mult_a_sym_cnt < 0 :
+                if mult_a >= min_range and mult_a <= max_range :
+                    mult_a_sym_cnt = cnt_idx
+
+            if mult_b_sym_cnt < 0 :
+                if mult_b >= min_range and mult_b <= max_range :
+                    mult_b_sym_cnt = cnt_idx
+                    
+            if rem_sym_cnt < 0 :
+                if remainder >= min_range and remainder <= max_range :
+                    rem_sym_cnt = cnt_idx
+                    
+            if mult_a_sym_cnt < 0 or mult_b_sym_cnt < 0 or rem_sym_cnt < 0 :
+                cnt_idx += 1
+            else:
+                """ All done break from while """
+                break
+            
+        """ Base symbols count """
+        total = mult_a_sym_cnt + mult_b_sym_cnt + rem_sym_cnt
+        print( "Base symbols count %2d : %2d,  %2d,  %2d, %2d" % (list_idx, mult_a_sym_cnt, mult_b_sym_cnt, rem_sym_cnt, total ), file=self.file_obj )
+            
+            
+    def __get_minimum_symbol_data(self, list_idx):
+        mult_a = self.number_list_obj.number_list[list_idx][0]
+        mult_b = self.number_list_obj.number_list[list_idx][1]
+        remainder = self.number_list_obj.number_list[list_idx][2]
+        print( "Minimum sysmbols %2d : %2d x %2d + %2d" % (list_idx, mult_a, mult_b, remainder ), file=self.file_obj )
+            
+            
+    def get_symbol_data(self):
+        file_name = str(self.number) + '.txt'
+        self.file_obj = open( file_name, 'w' )
+        
+        """ Set up the range list for the base """
+        base = self.min_base
+        self.__symbol_count_for_base(base)
+        self.__get_symbol_count()
+        
+        self.file_obj.close()
+        
+    def print_symbols_to_file(self):
+        """ Use files for now - see how it goes """
+        """ TODO : use MongoDB - seems a likely candidate """
+        """ TODO : create a class that can be used both for MongoDB and file operations """
+        file_name = str(self.number) + '.txt'
+        self.file_obj = open( file_name, 'w' )
+        self.__get_base_symbol_data(0)
+        self.file_obj.close()
+        
+    def set_number(self, number):
+        self.number = number
+        self.__get_base_range()
+                
+    def print_list(self):
+        print("Number %d, min base %d, max base %d" % (self.number, self.min_base, self.max_base))
+        
+    def print_range(self, base):
+        self.__symbol_count_for_base(base)
+        range_length = len(self.symbol_range_list)
+        range_idx = 1
+        while range_idx < range_length:
+            print("Base %d: symbol count %d, min range %d max range %d" % (base, range_idx, self.symbol_range_list[range_idx][0], self.symbol_range_list[range_idx][1] ))
+            range_idx += 1
+
+    def print_range_temp(self, base):
+        self.__symbol_count_for_base(base)
+            
+if __name__ == "__main__":
+    aSymbolObj = SymbolCount(31)
+    """ aSymbolObj.print_list() """
+    """ aSymbolObj.set_number(54) """
+    aSymbolObj.print_list()
+    #aSymbolObj.print_range(31)
+    
+    #aSymbolObj.print_symbols_to_file()
+    aSymbolObj.get_symbol_data()
